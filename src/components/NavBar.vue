0449fc7d4a0d3ca7c65718e1f248db9056f14ecd
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-22 18:22:49 +0100

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,98 @@
+<template>
+<div id="navbar">
+  <v-app-bar fixed app flat >
+    <div id="title-sub-container">
+        <h1 id="title">Pieter's Piano</h1>
+        <small id="sub">Professional Piano Lessons</small>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <a class="nav-link" href="/">Home</a>
+    <a class="nav-link" href="/about">Meet Your Teacher</a>
+    <v-menu
+      open-on-hover
+      top
+      offset-y
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <a class="nav-link" v-bind="attrs" v-on="on">
+          Content
+        </a>
+      </template>
+
+      <v-list flat>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index" 
+        >
+          <a class="nav-link" :href="item.to">{{item.title}}</a>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <div class="custom-spacer"/>
+
+    <a class="nav-link" href="/lessons">Lessons</a>
+    <a class="nav-link" href="/contact">Contact</a>
+    <a class="nav-link" href="/payments">Payments</a>
+
+  </v-app-bar>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+      items: [
+      {
+        title: "Recitals",
+        to: "/recitals",
+      },
+      {
+        title: "Past and Present Pupils",
+        to: "/pupils",
+      },
+      {
+        title: "Testimonials",
+        to: "/testimonials",
+      },
+    ],
+    }),
+  methods: {
+    onScroll(e) {
+      this.is_transparent = !(e.target.scrollTop > 0);
+    }
+  },
+}
+</script>
+
+<style scoped>
+.nav-link{
+    color: black;
+    text-decoration: none;
+    margin: 0 10px 0 10px;
+}
+.nav-link:hover{
+    color: rgb(65, 136, 202);
+}
+#title{
+    padding: 0px;
+    margin: 10px 0px -10px 0px;
+}
+#sub{
+    padding: 0px 0px 10px 0px;
+    margin: 0px;
+}
+#title-sub-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+}
+.custom-spacer {
+    width: 75px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,50 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/lessons',
+    name: 'Lessons',
+
+    component: () => import(/* webpackChunkName: "lessons" */ '../views/Lessons.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
+  },
+  {
+    path: '/payments',
+    name: 'Payments',
+
+    component: () => import(/* webpackChunkName: "payments" */ '../views/Payment.vue')
+  },
+  {
+    path: '/recitals',
+    name: 'Recitals',
+
+    component: () => import(/* webpackChunkName: "recitals" */ '../views/Recitals.vue')
+  },
+  {
+    path: '/pupils',
+    name: 'Pupils',
+
+    component: () => import(/* webpackChunkName: "pupils" */ '../views/Pupils.vue')
+  },
+  {
+    path: '/testimonials',
+    name: 'Testimonials',
+
+    component: () => import(/* webpackChunkName: "testimonials" */ '../views/Testimonials.vue')
+  },
+  {
+    path: "*",
+    name: "404",
+
+     component: () => import(/* webpackChunkName: "404" */ '../views/404.vue')
   }
 ]
 
