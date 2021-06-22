ac99325488ffc529e45c0885791aedf1ac1fd25d
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-22 22:37:15 +0100

    improvement to navbar (added :hover styling to nav links)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,51 +1,43 @@
 <template>
-<div id="navbar">
-  <v-app-bar fixed app flat >
-    <div id="title-sub-container">
+  <div id="navbar">
+    <v-app-bar fixed app flat>
+      <a href="/" id="title-sub-container">
         <h1 id="title">Pieter's Piano</h1>
         <small id="sub">Professional Piano Lessons</small>
-    </div>
+      </a>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <a class="nav-link" href="/">Home</a>
-    <a class="nav-link" href="/about">Meet Your Teacher</a>
-    <v-menu
-      open-on-hover
-      top
-      offset-y
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <a class="nav-link" v-bind="attrs" v-on="on">
-          Content
-        </a>
-      </template>
+      <a class="nav-link" href="/">Home</a>
+      <a class="nav-link" href="/about">Meet Your Teacher</a>
+      <v-menu open-on-hover top offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <a class="nav-link" v-bind="attrs" v-on="on">
+            Content
+          </a>
+        </template>
 
-      <v-list flat>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index" 
-        >
-          <a class="nav-link" :href="item.to">{{item.title}}</a>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+        <v-list flat>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <a class="nav-link" :href="item.to">{{ item.title }}</a>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-    <div class="custom-spacer"/>
+      <div class="custom-spacer" />
 
-    <a class="nav-link" href="/lessons">Lessons</a>
-    <a class="nav-link" href="/contact">Contact</a>
-    <a class="nav-link" href="/payments">Payments</a>
-
-  </v-app-bar>
-</div>
+      <a class="nav-link" href="/lessons">Lessons</a>
+      <a class="nav-link" href="/contact">Contact</a>
+      <a class="nav-link" href="/payments">Payments</a>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Navbar",
   data: () => ({
-      items: [
+    items: [
       {
         title: "Recitals",
         to: "/recitals",
@@ -59,40 +51,44 @@ export default {
         to: "/testimonials",
       },
     ],
-    }),
+  }),
   methods: {
     onScroll(e) {
       this.is_transparent = !(e.target.scrollTop > 0);
-    }
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-.nav-link{
-    color: black;
-    text-decoration: none;
-    margin: 0 10px 0 10px;
+.nav-link {
+  color: black;
+  text-decoration: none;
+  margin: 0 10px 0 10px;
 }
-.nav-link:hover{
-    color: rgb(65, 136, 202);
+.nav-link:hover {
+  color: rgb(65, 136, 202);
 }
-#title{
-    padding: 0px;
-    margin: 10px 0px -10px 0px;
+.nav-link:active {
+  color: rgb(46, 102, 206);
 }
-#sub{
-    padding: 0px 0px 10px 0px;
-    margin: 0px;
+#title {
+  padding: 0px;
+  margin: 10px 0px -10px 0px;
+}
+#sub {
+  padding: 0px 0px 10px 0px;
+  margin: 0px;
 }
 #title-sub-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
+  color: black;
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .custom-spacer {
-    width: 75px;
+  width: 75px;
 }
 </style>
