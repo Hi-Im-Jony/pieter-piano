50a796050e6f81b53148289d7914afc517f59f1d
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-23 20:46:47 +0100

    added pros component to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <parallax-pic />
+  <div>
+    <parallax-pic />
+    <pros />
+  </div>
 </template>
 
 <script>
 import ParallaxPic from "../components/ParallaxPic.vue";
+import Pros from "../components/pros.vue";
 
 export default {
   name: "Home",
 
   components: {
     ParallaxPic,
+    Pros,
   },
 };
 </script>
