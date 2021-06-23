f2a9e1b61358874912f55adcf85708ecb8a125e4
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-23 20:46:33 +0100

    created pros component

---
+++ src/components/pros.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="main-div">
+    <h2>Experienced Teacher</h2>
+    <p>
+      Nothing can bea experience, and I have plenty of it. After over thiry
+      years of teaching music, you can rely on me to give you a truly fulfilling
+      learning experience.
+    </p>
+    <v-img />
+    <h2>Indisputable Success Rate</h2>
+    <p>
+      I love the privilege and the challenge that it is to bring out the best in
+      every student
+      <br />
+      That is why over years, I average a rate of eighty-percent Distinctions
+      from my students taking RIAM and TCM examinations
+    </p>
+    <v-img />
+    <h2>Master Piano Lessons</h2>
+    <p>
+      So what are you waiting for?
+      <br />
+      Let me bring the best out of you, and let me teach you the wonderful art
+      of piano playing, from the comfort of your own home, or mine.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#main-div {
+  background-color: lightblue;
+}
+</style>
