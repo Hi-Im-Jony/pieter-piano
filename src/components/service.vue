86b4953027de01a63f26b7f2951d21780bb6f008
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 14:31:38 +0100

    created the service component

---
+++ src/components/service.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="service">
+    <h3>{{ title }}</h3>
+    <div class="line"></div>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Service",
+  props: ["title", "text"],
+};
+</script>
+
+<style scoped>
+.service {
+  text-align: center;
+  width: 100%;
+  margin: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.line {
+  height: 1px;
+  width: 55%;
+  border-top: solid white;
+  border-width: 1px;
+  margin: 0 0 20px 0;
+}
+</style>
