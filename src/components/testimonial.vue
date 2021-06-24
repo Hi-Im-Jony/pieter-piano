1a69b67efd84c78bc35062a424c31a65cee613cc
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 20:48:42 +0100

    prototype of testimonials page

---
+++ src/components/testimonial.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="testimonial">
+    <div class="author-container">
+      <h2 class="author">{{ test.author }}</h2>
+      <i class="description">{{ test.description }}</i>
+    </div>
+    <div class="test-text-container">
+      <div v-if="!readMore" class="short-text">
+        {{ test.text.slice(0, 200) }}...
+        <br />
+        <a v-on:click="readMore = true"> Read more</a>
+      </div>
+      <div v-else>
+        {{ test.text }}
+        <br />
+        <a v-on:click="readMore = false">See less</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Testimonial",
+  props: ["test"],
+  data() {
+    return {
+      readMore: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.testimonial {
+  background: rgb(17, 17, 17);
+  box-shadow: 0px 0px 5px 2px black;
+  display: flex;
+  justify-content: left;
+  padding: 20px;
+  margin: 40px;
+}
+
+.author-container {
+  border-right: solid white;
+  padding: 0 20px 0 0;
+  margin: 0 20px 0 0;
+}
+
+.test-text-container {
+  width: 500px;
+  text-align: center;
+}
+</style>
---
+++ src/views/Testimonials.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="testimonials-page">
+    <h1 id="page-title">Testimonials</h1>
+    <testimonial :test="tests[0]" />
+    <testimonial :test="tests[1]" />
+  </div>
+</template>
+
+<script>
+import Testimonial from "../components/testimonial.vue";
+import testimonials from "../testimonials/testimonials.js";
+export default {
+  name: "Testimonials",
+  components: { Testimonial },
+  data() {
+    return {
+      tests: testimonials,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#testimonials-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#page-title {
+  margin: 20px;
+}
+</style>
