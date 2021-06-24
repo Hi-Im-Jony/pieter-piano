22605c70757934ab5fb2fa8fb5b198bd9d89d22b
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 15:07:53 +0100

    created contact page

---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="contact-page">
+    <h1>Contact Me Today</h1>
+    <my-footer />
+  </div>
+</template>
+
+<script>
+import MyFooter from "../components/my-footer.vue";
+export default {
+  name: "contact",
+  components: { MyFooter },
+};
+</script>
+
+<style scoped>
+#contact-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
