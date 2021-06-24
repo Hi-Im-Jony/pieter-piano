843624199a53fedb68b50f49011ee66bf5987e5b
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 12:48:28 +0100

    created my footer

---
+++ src/components/my-footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <small>&#169; All Rights Reserved</small>
+
+    <small>
+      Designed by
+      <a href="https://corneljonathan.website" target="_blank">
+        Cornel Jonathan Web Design
+      </a>
+    </small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<style scoped>
+footer {
+  color: white;
+  background-color: rgb(36, 33, 33);
+  text-align: center;
+  position: absolute;
+  width: 100vw;
+  padding: 10px;
+}
+small {
+  margin: 0 20px 0 10px;
+}
+</style>
