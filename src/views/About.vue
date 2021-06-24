3b41884d706455b5dc870ae88adbab6b2c31029c
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 13:14:16 +0100

    started work on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 id="page-title">
+      Book Your Lesson Today
+    </h1>
+    <img id="lesson pic" src="../assets/lesson-pic.png" alt="" />
   </div>
 </template>
 
 <style scoped>
 .about {
   height: 200vh;
-  background-color: lightcoral;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
