db44835b3aac4775f97905046355af498ca5b6e4
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-24 14:31:50 +0100

    put services together in their own component

---
+++ src/components/services.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="services-container">
+    <h1>Services</h1>
+    <service
+      title="Piano Lessons"
+      text="Learn the basics of piano playing, then master them. Students of all ages and levels are welcome."
+    />
+    <service
+      title="Theory Lessons"
+      text=" From learning to read basic rhythms, to identifying cadences, there is always more theory to learn."
+    />
+
+    <service
+      title="Online Lessons"
+      text="Can’t join me in person? That’s okay! I offer online lessons too!"
+    />
+  </div>
+</template>
+
+<script>
+import Service from "./service.vue";
+export default {
+  name: "Services",
+  components: { Service },
+};
+</script>
+
+<style scoped>
+#services-container {
+  background: transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+}
+</style>
