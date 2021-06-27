40c8c46024436805ea942662efb3950d5abbd357
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-27 15:49:06 +0100

    starting payment page

---
+++ src/views/Payment.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="payment-page">
+    <h1 id="page-title">Make A Payment</h1>
+    <p id="page-descriptor">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "payments",
+  components: {},
+};
+</script>
+
+<style scoped>
+#payment-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+#page-title {
+  padding: 10px;
+  margin: 0 0 15px 0;
+  border-bottom: solid white;
+  width: 20%;
+  text-align: center;
+}
+#page-descriptor {
+  width: 40%;
+  text-align: center;
+}
+</style>
