11b32fc3863a83b08d16834bd3865f969c1c0fd8
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-28 10:52:28 +0100

    created product component

---
+++ src/components/product.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="product">
+    <h1 class="prod-title">{{ title }}</h1>
+    <img
+      class="prod-img"
+      :src="require('@/assets/images/' + image + '')"
+      alt=""
+    />
+    <p class="prod-description">{{ description }}</p>
+    <v-btn dark class="pay-btn" :href="link">Pay now</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  props: ["title", "image", "description", "link"],
+};
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 400px;
+  background-color: rgb(7, 7, 7);
+  box-shadow: 0px 0px 5px 0px rgb(0, 0, 0);
+  text-align: center;
+  padding: 20px;
+  margin: 20px;
+}
+.prod-img {
+  width: 200px;
+  margin: 20px;
+}
+
+.prod-description {
+  margin: 10px;
+}
+
+.pay-btn {
+  padding: 10px;
+}
+</style>
