983419175fbdd14e5da21ae4cdaa1eadbd977e7b
Hi-Im-Jony <jccicai@yahoo.ie>
2021-06-28 10:52:38 +0100

    payment page prototype

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -2,16 +2,57 @@
   <div id="payment-page">
     <h1 id="page-title">Make A Payment</h1>
     <p id="page-descriptor">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
-      nobis veniam quos quam porro molestias.
+      Easy and hassle free online payments are here!
+      <br />
+      <br />
+      Select the service you wish to pay for, fill in your details, and then
+      proceed with the payment. Once you do so, you recieve an email comfirming
+      you payment.
     </p>
+    <div id="product-container">
+      <product
+        title="30-min session"
+        image="experience-pic.jpg"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias."
+        link="#"
+      />
+      <product
+        title="30-min session"
+        image="experience-pic.jpg"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias."
+        link="#"
+      />
+      <product
+        title="30-min session"
+        image="experience-pic.jpg"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias."
+        link="#"
+      />
+      <product
+        title="30-min session"
+        image="experience-pic.jpg"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      nobis veniam quos quam porro molestias."
+        link="#"
+      />
+    </div>
+    <my-footer />
   </div>
 </template>
 
 <script>
+import MyFooter from "../components/my-footer.vue";
+import Product from "../components/product.vue";
 export default {
   name: "payments",
-  components: {},
+  components: { Product, MyFooter },
 };
 </script>
 
@@ -21,6 +62,15 @@ export default {
   flex-direction: column;
   align-items: center;
   padding: 20px;
+  text-align: center;
+  background: linear-gradient(
+    180deg,
+    black,
+    rgb(51, 49, 49),
+    rgb(51, 49, 49),
+    black
+  );
+  min-height: 100vh;
 }
 #page-title {
   padding: 10px;
@@ -32,5 +82,10 @@ export default {
 #page-descriptor {
   width: 40%;
   text-align: center;
+}
+#product-container {
+  display: flex;
+  align-items: center;
+  padding: 40px;
 }
 </style>
